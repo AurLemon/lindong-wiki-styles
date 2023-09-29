0b289e8bbb34c609d936682350b6b96d61158c3d
AurLemon <67463076+AurLemon@users.noreply.github.com>
2023-09-29 22:18:57 +0800

    添加功能

--- Module_LineList/Bus.lua
+++ Module_LineList/Bus.lua
@@ -1,46 +1,80 @@
+--
+-- 柠檬写的 230909
+-- For 临东定制
+--
+
 local p = {}
 
 function p.infobox(frame)
-    local args = frame:getParent().args
-    local lineName = args.Name or ""
-    local lineColor = args.Color or "gray"
-    local company = args.Company or ""
-    local ticket = args.Ticket or ""
-    local firstTrain = args.FirstTrain or ""
-    local lastTrain = args.LastTrain or ""
-
-    local output = mw.html.create("div")
-    output:addClass("infobox-card")
-
-    local header = output:tag("div"):addClass("header")
-    header:tag("div"):addClass("line-name"):wikitext(lineName)
-
-    local detail = header:tag("div"):addClass("detail")
-    detail:tag("div"):addClass("company"):wikitext(company)
-
-    local time = header:tag("div"):addClass("time")
-    time:tag("div"):addClass("first-train"):wikitext(firstTrain)
-    time:tag("div"):addClass("last-train"):wikitext(lastTrain)
-    time:tag("div"):addClass("ticket"):wikitext(ticket)
-
-    local stationList = output:tag("ul"):addClass("station-list")
+	local args = frame:getParent().args
+	local lineName = args.Name
+	local lineColor = args.Color
+	local company = args.Company
+	local ticket = args.Ticket
+	local firstTrain = args.FirstTrain
+	local lastTrain = args.LastTrain
+	
+	local output = mw.html.create("div")
+	output:addClass("infobox-card")
+	
+	if args.Clear ~= nil then
+		output:cssText('clear: unset;')
+	end
+	
+	local header = output:tag("div"):addClass("header")
+	header:tag("div"):addClass("line-name"):wikitext(lineName)
+	
+	local detail = header:tag("div"):addClass("detail")
+	detail:tag("div"):addClass("company"):wikitext(company)
+	
+	local time = header:tag("div"):addClass("time")
+	time:tag("div"):addClass("first-train"):wikitext(firstTrain)
+	time:tag("div"):addClass("last-train"):wikitext(lastTrain)
+	time:tag("div"):addClass("ticket"):wikitext(ticket)
+	
+	local stationList = output:tag("ul"):addClass("station-list")
+	if args.Direction then
+		local direction = args.Direction
+		stationList:tag("div"):addClass("direction"):wikitext(direction)
+	end
 	stationList:wikitext("<div class='line' style='background-color:" .. lineColor .. ";'></div>")
-
-    local stationIndex = 1
-    local insertedLine = false
-    while args["Sta" .. stationIndex] do
-        local stationInfo = args["Sta" .. stationIndex]
-        local staName = mw.ustring.match(stationInfo, "([^|]+)")
-        
-        if staName then
-            local li = stationList:tag("li")
-            local stationDiv = li:tag("div"):addClass("name"):wikitext(mw.text.unstrip(staName))
-        end
-
-        stationIndex = stationIndex + 1
-    end
-
-    return tostring(output)
+	
+	local maxStationIndex = 0
+	for key, _ in pairs(args) do
+		local stationIndex = tonumber(key:match("^Sta(%d+)$"))
+		if stationIndex and stationIndex > maxStationIndex then
+			maxStationIndex = stationIndex
+		end
+	end
+	
+	for stationIndex = 1, maxStationIndex do
+		local stationInfo = args["Sta" .. stationIndex]
+		if stationInfo then
+			local li = stationList:tag("li")
+			local stationDiv = li:tag("div"):addClass("name")
+			local stationDivCircle = stationDiv:tag("div"):addClass("circle"):cssText('border: 4px solid ' .. lineColor .. ';')
+			local expandedText = frame:preprocess(stationInfo)
+			stationDiv:wikitext(expandedText)
+			
+			-- 未开放
+			if args["Sta" .. stationIndex .. "Unopen"] then
+				stationDiv:cssText('color: var(--color-base--subtle);')
+				stationDivCircle:cssText('border: 4px solid var(--color-surface-4);')
+			end
+			
+			-- 单向运营
+			local way = args["Sta" .. stationIndex .. "Way"]
+			if way then
+				if string.lower(way) == "up" then
+					stationDiv:addClass("dir-tag__up")
+				elseif string.lower(way) == "down" then
+					stationDiv:addClass("dir-tag__down")
+				end
+			end
+		end
+	end
+	
+	return tostring(output)
 end
 
 return p
